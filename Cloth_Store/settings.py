0afae9f40cb20666a9f0c6f1f89a882b3a378666
Nasirul-Islam <nasirulislam866@gmail.com>
2023-09-03 09:59:35 +0600

    static root added

--- Cloth_Store/settings.py
+++ Cloth_Store/settings.py
@@ -121,12 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-# define static directory
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
